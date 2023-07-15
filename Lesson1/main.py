f273af60d6ba964c2abd969c150ae7de9a642649
edukambarov <edukambarovgb@yandex.ru>
2023-07-15 10:34:11 +0300

    Seminar1 python

---
+++ Lesson1/main.py
@@ -0,0 +1,36 @@
+# n = "kv'/kv"
+# print(type(n))
+# комментировать строки
+# n = "kvkv"
+# n1 = 'strstr'
+
+# a = 2
+# b = 1.39
+# c = 'hello'
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a,b,c))
+
+# print ("Введите первое число: ")
+# a = int(input())
+# b = int(input('Введите второе число: '))
+# print (a,' + ',b,' = ',a+b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(type(c))
+# c = str(c)
+# print((c)+"89")
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# a = 5.8945
+# b = 4.2134
+# print(round((a*b),2))
+
