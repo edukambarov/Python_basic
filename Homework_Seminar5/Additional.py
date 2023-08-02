7892d12644b5f45a71bf04b4b89351fd50451939
edukambarov <edukambarovgb@yandex.ru>
2023-08-02 11:46:08 +0300

    Seminar6

---
+++ Homework_Seminar5/Additional.py
@@ -0,0 +1,13 @@
+# list_1 = ['в', '5', 'часов', '17','минут', 'температура', 'воздуха', 'была', '+5','градусов']
+
+# for i in range(len(list_1)):
+#     if int(list_1[i].replace("'","")) > 0:
+#         list_1[i] = f'"{list_1[i]}"' if int(list_1[i]) > 0 else f'"0{list_1[i]}"'
+    
+
+a = '5'
+b = 'часов'
+print(type(a.replace("'","")))
+print(type(b.replace("'","")))
+
+
---
+++ Task43.py
@@ -0,0 +1,20 @@
+# Задача №43. Решение в группах
+# Дан список чисел. Посчитайте, сколько в нем пар
+# элементов, равных друг другу. Считается, что любые
+# два элемента, равные друг другу образуют одну пару,
+# которую необходимо посчитать. Вводится список
+# чисел. Все числа списка находятся на разных
+# строках.
+
+lst = [5,5,5,3,3,3,3,2,2,2,2]
+#print(sum([lst.count(item)//2 for item in set(lst)]))
+
+from random import randint as rd
+count1 = int(input("Введите кол-во элементов  массива: "))
+list1 = [rd(1,5) for _ in range(count1)]
+print(list1)
+pairs = 0
+for item in set(list1):
+    print(item, list1.count(item))
+    pairs += list1.count(item) // 2
+print(pairs)
