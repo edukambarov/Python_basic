2f0b6dab0272a5e0b4c9459a31e4d7376442cb7e
edukambarov <edukambarovgb@yandex.ru>
2023-08-21 11:40:50 +0300

    task5 is corrected

--- Paradigma/Para_Sem_1_Homework/para_task5.py
+++ Paradigma/Para_Sem_1_Homework/para_task5.py
@@ -10,7 +10,7 @@ def find_prime_numbers_imperative(n):
     numbers = [x for x in range(1,n+1)]
     prime_numbers = []
     for x in numbers:
-        for y in range(2, x//2): 
+        for y in range(2, x): 
             if (x % y) == 0: 
                 break 
         else: 
