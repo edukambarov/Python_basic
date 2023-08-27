120946e6fe813253a454e77f4a0623c96375eeac
edukambarov <edukambarovgb@yandex.ru>
2023-08-27 21:36:47 +0300

    Phonebook with modules

--- Seminar9/model.py
+++
@@ -1,9 +0,0 @@
-phone_book ={}
-PATH = 'phones.txt'
-
-def open_file():
-    global phone_book
-    with open(PATH, 'r', encoding = 'unt-8') as file:
-        data = file.readlines()
-    for contact in enumerate(data,1):
-        contact = contact.strip().s
